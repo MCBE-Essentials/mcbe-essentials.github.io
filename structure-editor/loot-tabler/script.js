fac04c98f19b38d543dd1cf50df010aae02cffb7
Glitch (mcbe-essentials) <none>
2022-04-18 20:31:32 +0000

    Structure to Function fill command update

---
+++ structure-editor/loot-tabler/script.js
@@ -0,0 +1,3 @@
+var structure = 'https://cdn.glitch.global/17ff8eee-9239-4ba0-8a5c-9263261550b5/bobby.mcstructure?v=1642358868820';
+
+var tileentities = Object.values(structure.value.structure.value.palette.value.default.value.block_position_data.value);
